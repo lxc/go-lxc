b6f95256b13696bfd5147fa7e1f38aacebd923bd
Syed <syed1.mushtaq@gmail.com>
2015-03-13 12:17:29 -0400

    Adding licence header to the config file

--- examples/config.go
+++ examples/config.go
@@ -1,3 +1,7 @@
+// Copyright © 2013, 2014, The Go-LXC Authors. All rights reserved.
+// Use of this source code is governed by a LGPLv2.1
+// license that can be found in the LICENSE file.
+
 // +build linux,cgo
 
 package main
