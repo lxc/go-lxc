28a40bd951fe860cc9869ebb06e96b07f8df1c76
S.Çağlar Onur <conur@vmware.com>
2016-01-07 13:30:51 -0600

    fix isGreaterEqualThanLXC11 and isGreaterEqualThanLXC20 tests

--- container.go
+++ container.go
@@ -72,11 +72,11 @@ func (c *Container) makeSure(flags int) error {
 		return ErrMethodNotAllowed
 	}
 
-	if flags&isGreaterEqualThanLXC11 != 0 && !(C.LXC_VERSION_MAJOR >= 1 && C.LXC_VERSION_MINOR >= 1) {
+	if flags&isGreaterEqualThanLXC11 != 0 && !(C.LXC_VERSION_MAJOR > 1 || C.LXC_VERSION_MAJOR >= 1 && C.LXC_VERSION_MINOR >= 1) {
 		return ErrNotSupported
 	}
 
-	if flags&isGreaterEqualThanLXC20 != 0 && !(C.LXC_VERSION_MINOR >= 2 && C.LXC_VERSION_MAJOR >= 0) {
+	if flags&isGreaterEqualThanLXC20 != 0 && !(C.LXC_VERSION_MAJOR > 2 || C.LXC_VERSION_MINOR >= 2 && C.LXC_VERSION_MAJOR >= 0) {
 		return ErrNotSupported
 	}
 
