3b71609f178b050aa529812e3bec849c89baedc3
Christian Brauner <christian.brauner@ubuntu.com>
2018-11-01 16:56:27 +0100

    bindings: add runtime version check
    Do not expose it as a public function just yet though.
    
    Needed-by: https://github.com/lxc/lxd/pull/5228
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- lxc-binding.go
+++ lxc-binding.go
@@ -19,6 +19,7 @@ import "C"
 import (
 	"fmt"
 	"runtime"
+	"strconv"
 	"strings"
 	"unsafe"
 )
@@ -244,9 +245,82 @@ func IsSupportedConfigItem(key string) bool {
 	return bool(C.go_lxc_config_item_is_supported(configItem))
 }
 
+// runtimeLiblxcVersionAtLeast checks if the system's liblxc matches the
+// provided version requirement
+func runtimeLiblxcVersionAtLeast(major int, minor int, micro int) bool {
+	version := Version()
+	version = strings.Replace(version, " (devel)", "-devel", 1)
+	parts := strings.Split(version, ".")
+	partsLen := len(parts)
+	if partsLen == 0 {
+		return false
+	}
+
+	develParts := strings.Split(parts[partsLen-1], "-")
+	if len(develParts) == 2 && develParts[1] == "devel" {
+		return true
+	}
+
+	maj := -1
+	min := -1
+	mic := -1
+
+	for i, v := range parts {
+		if i > 2 {
+			break
+		}
+
+		num, err := strconv.Atoi(v)
+		if err != nil {
+			return false
+		}
+
+		switch i {
+		case 0:
+			maj = num
+		case 1:
+			min = num
+		case 2:
+			mic = num
+		}
+	}
+
+	/* Major version is greater. */
+	if maj > major {
+		return true
+	}
+
+	if maj < major {
+		return false
+	}
+
+	/* Minor number is greater.*/
+	if min > minor {
+		return true
+	}
+
+	if min < minor {
+		return false
+	}
+
+	/* Patch number is greater. */
+	if mic > micro {
+		return true
+	}
+
+	if mic < micro {
+		return false
+	}
+
+	return true
+}
+
 // HasApiExtension returns true if the extension is supported.
 func HasApiExtension(extension string) bool {
-	apiExtension := C.CString(extension)
-	defer C.free(unsafe.Pointer(apiExtension))
-	return bool(C.go_lxc_has_api_extension(apiExtension))
+	if runtimeLiblxcVersionAtLeast(3, 1, 0) {
+		apiExtension := C.CString(extension)
+		defer C.free(unsafe.Pointer(apiExtension))
+		return bool(C.go_lxc_has_api_extension(apiExtension))
+	}
+	return false
 }
